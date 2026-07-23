aaefef6b7d7d1dcc2ad44ea55254d957b939e154
HuangBlac <1239256942@qq.com>
2026-07-23 14:38:18 +0800

    项目迁移到了wsl之中，可以避免一些奇怪的Bug

---
+++ MyTransformer.py
@@ -0,0 +1,13 @@
+'''
+在这个部分中，我将会从开始动手实现一个transformer模块
+不依赖于任何大模型
+'''
+import torch
+from torch import nn
+
+class Mytransformer(nn.Module):
+    def __init__(self):
+        super()
+
+    def forward(self):
+        pass
